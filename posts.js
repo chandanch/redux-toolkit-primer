7b9c226a39301874e9a6f8a4e63d39fd3d95e83f
chandanch <chandan.cec107@gmail.com>
2023-05-26 00:02:11 +0530

    createSlice and use of extra reducers

--- posts.js
+++ posts.js
@@ -1,4 +1,4 @@
-const { createAsyncThunk } = require('@reduxjs/toolkit');
+const { createAsyncThunk, createSlice } = require('@reduxjs/toolkit');
 const axios = require('axios');
 
 // 1. create an initial state
@@ -18,6 +18,9 @@ const initialState = {
  * accepts a Redux action type string and a callback function(payload creator callback) that should return a promise
  * generates action types that resembles the promise lifcycle(pending, fulfilled, rejected)
  * these action types are prefixed with an action type string
+ * 
+ * A string that will be used to generate additional Redux action type constants, representing the lifecycle of an async request:
+
  *
  * createAsyncThunk() returns a standard Redux thunk action creator.
  */
@@ -25,4 +28,28 @@ const initialState = {
 const fetchPosts = createAsyncThunk('posts/fetch', async () => {
 	const data = await axios.get('https://jsonplaceholder.typicode.com/posts');
 	return data;
+});
+
+// 3. create a slice
+const postSlice = createSlice({
+	name: 'posts',
+	initialState,
+	// extraReducers must be used to handle async actions
+	extraReducers: (builder) => {
+		// used to handle lifecycle - pending, fulfilled, rejected
+		builder.addCase(fetchPosts.pending, (state) => {
+			state.loading = true;
+		});
+
+		builder.addCase(fetchPosts.fulfilled, (state, action) => {
+			state.posts = action.payload;
+			state.loading = false;
+		});
+
+		builder.addCase(fetchPosts.rejected, (state, action) => {
+			state.posts = [];
+			state.error = action.payload;
+			state.loading = false;
+		});
+	},
 });
